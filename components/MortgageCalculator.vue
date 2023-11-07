6a306343ade0f405c4aa2c8446d2b761ec80cb59
Shukrullo2799 <104069212+Shukrullo2799@users.noreply.github.com>
2023-11-07 20:03:00 +0500

    slider

--- components/MortgageCalculator.vue
+++ components/MortgageCalculator.vue
@@ -1,12 +1,56 @@
 <template>
-  <div>
-    <h1>Mortgage Calculator</h1>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere deleniti expedita aperiam
-      ratione consequuntur libero, recusandae id nostrum quibusdam suscipit amet voluptates aut ea
-      vitae sint! Laboriosam cumque a recusandae.
-    </p>
+  <div class="calc">
+    <button class="calc-btn">Ипотечный калькулятор</button>
+    <div>
+      <label for="price">Стоимость недвижимости</label>
+      <div>
+        <div>
+          <input type="range" id="price" name="price" min="1000000" max="10000000" />
+          <input type="text" id="price" name="price" />
+        </div>
+        <div>
+          <input type="range" id="price2" name="price2" min="1000000" max="10000000" />
+          <input type="text" id="price2" name="price2" />
+        </div>
+      </div>
+    </div>
+    <div>
+      <input type="checkbox" v-model="form.home" />
+      <label for="home">Квартира</label>
+      <input type="checkbox" v-model="form.apartment" />
+      <label for="apartment">Апартамент</label>
+    </div>
+    <div>
+      <label for="down_payment">Первоначальный взнос, ₽</label>
+      <div>
+        <div>
+          <span>
+            {{ parseInt(maxPrice * (form.down_persent / 100)).toLocaleString("ru-RU") }}
+          </span>
+          <span>{{ form.down_persent }} </span>
+        </div>
+        <input type="range" v-model="form.down_persent" />
+      </div>
+      <div>
+        <label for="credit_term">Срок выплат</label>
+        <div>
+          <span>{{ Math.round(form.credit_term / 12) }} лет </span>
+          <input type="range" v-model="form.credit_term" max="180" min="12" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+const maxPrice = 5000000;
+const form = ref({
+  price: 5.6,
+  price2: 5.6,
+  home: false,
+  apartment: false,
+  down_persent: 20,
+  credit_term: 60,
+});
+form.value.down_payment = parseInt(maxPrice * (form.value.down_persent / 100));
+</script>
--- components/SliderCard.vue
+++ components/SliderCard.vue
@@ -1,12 +1,37 @@
 <template>
-  <div>
-    <h1>SliderCard</h1>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere deleniti expedita aperiam
-      ratione consequuntur libero, recusandae id nostrum quibusdam suscipit amet voluptates aut ea
-      vitae sint! Laboriosam cumque a recusandae.
+  <div
+    class="slider_item"
+    :style="{
+      backgroundImage: `url(/${item.img}.png)`,
+    }"
+  >
+    <p class="slider_item-title">{{ item.title }}</p>
+    <p class="slider_item-text">
+      {{ item.text }}
     </p>
+    <span class="slider_item-btn">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="65"
+        height="66"
+        viewBox="0 0 65 66"
+        fill="none"
+      >
+        <circle cx="32.5" cy="33.3125" r="32.5" fill="#083E4C" />
+        <path
+          d="M40.3889 33.1687C41.0556 33.5536 41.0556 34.5158 40.3889 34.9007L30.3889 40.6742C29.7222 41.0591 28.8889 40.578 28.8889 39.8082L28.8889 28.2612C28.8889 27.4914 29.7222 27.0103 30.3889 27.3952L40.3889 33.1687Z"
+          fill="white"
+        />
+      </svg>
+    </span>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+const { item } = defineProps({
+  item: {
+    type: Object,
+    default: () => ({}),
+  },
+});
+</script>
