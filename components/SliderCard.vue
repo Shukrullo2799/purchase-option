bfc564414ee0ec954859b48f162680ca4bd613f2
Shukrullo2799 <104069212+Shukrullo2799@users.noreply.github.com>
2023-11-07 16:15:45 +0500

    push

---
+++ components/SliderCard.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>SliderCard</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere deleniti expedita aperiam
+      ratione consequuntur libero, recusandae id nostrum quibusdam suscipit amet voluptates aut ea
+      vitae sint! Laboriosam cumque a recusandae.
+    </p>
+  </div>
+</template>
+
+<script setup></script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,12 @@
 <template>
-  <div>
-    <h1>Home</h1>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere deleniti expedita aperiam
-      ratione consequuntur libero, recusandae id nostrum quibusdam suscipit amet voluptates aut ea
-      vitae sint! Laboriosam cumque a recusandae.
-    </p>
+  <div class="h-screen">
+    <Slider />
+    <div class="flex">
+      <MortgageCalculator />
+      <MortgageInfo />
+    </div>
   </div>
 </template>
 
 <script setup></script>
+<style scoped></style>
